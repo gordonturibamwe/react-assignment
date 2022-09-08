380f6f87c113fc68b5d09e1352b2dcfcefc51a1e
User <zhedra@zhedra-2-mini.myfiosgateway.com>
2022-09-08 12:03:28 -0400

    Adding groups

--- src/version_1/components/GroupJoinButtonsComponent.js
+++ src/version_1/components/GroupJoinButtonsComponent.js
@@ -3,18 +3,19 @@ import React from 'react'
 export default function GroupJoinButtonsComponent({...props}) {
   return (
     <div className='flex-none'>
-      {/* To be used on loading */}
-      {props.group.user_id != props.currentUser.id && !props.group?.user_exists_in_group?.request_accepted && props.group.group_access == 'is_public' &&
-      <button onClick={(event) => props.joinGroup(event, props.group.id)} className="rounded-[4px] shadow-sm border border-gray-300 hover:text-gray-600 bg-white px-4 py-2 text-gray-600 block text-sm font-medium flex-none">1 Join Group</button>}
-      {props.group.user_id != props.currentUser.id && !props.group?.user_exists_in_group?.request_accepted && props.group.group_access == 'is_private' &&
-      <button onClick={(event) => props.requestToJoinPrivateGroup(event, props.group.id)}  className="rounded-[4px] shadow-sm border border-gray-300 hover:text-gray-600 bg-white px-4 py-2 text-gray-600 block text-sm font-medium flex-none">Request to Join</button>}
+      {props.currentUser.id != props.group.user_id && props.group.user_id == props.group?.user_exists_in_group?.user_id && props.group?.user_exists_in_group?.request_accepted && props.group.group_access == 'is_public' &&
+        <button onClick={(event) => props.joinGroup(event, props.group.id)} className="rounded-[4px] shadow-sm border border-gray-300 hover:text-gray-600 bg-white px-4 py-2 text-gray-600 block text-sm font-medium flex-none">Join Group</button>}
 
-      {/* To be used on websockets */}
-      {props.group.user_id != props.currentUser.id && props.group?.user_exists_in_group?.request_accepted && props.group.group_access == 'is_public' &&
-      <button onClick={(event) => props.joinGroup(event, props.group.id)} className="rounded-[4px] shadow-sm border border-gray-300 hover:text-gray-600 bg-white px-4 py-2 text-gray-600 block text-sm font-medium flex-none">2 Join Group</button>}
-      {props.group.user_id != props.currentUser.id && props.group?.user_exists_in_group?.request_accepted && props.group.group_access == 'is_private' &&
-      <button onClick={(event) => props.requestToJoinPrivateGroup(event, props.group.id)}  className="rounded-[4px] shadow-sm border border-gray-300 hover:text-gray-600 bg-white px-4 py-2 text-gray-600 block text-sm font-medium flex-none">Request to Join</button>}
+      {props.currentUser.id != props.group.user_id &&
+      props.group?.user_exists_in_group == null &&
+      props.group.group_access == 'is_private' ?
+        <button onClick={(event) => props.requestToJoinPrivateGroup(event, props.group.id)} className="rounded-[4px] shadow-sm border border-gray-300 hover:text-gray-600 bg-white px-4 py-2 text-gray-600 block text-sm font-medium flex-none">Request to Join</button> :
+      props.currentUser.id != props.group.user_id &&
+      !props.group?.user_exists_in_group?.request_accepted &&
+      props.group.group_access == 'is_private' ?
+        <button className="px-4 py-2 text-gray-400 block font-normal text-xs flex-none">Waiting Approval</button> :
+        <span></span>
+      }
     </div>
   )
 }
-
